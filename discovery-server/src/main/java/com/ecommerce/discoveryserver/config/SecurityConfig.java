e7fb94ba77df0a04b7e2a1cbd01360fc564f561f
thien <nguyenthanhthien0611@gmail.com>
2023-03-03 14:26:20 +0700

    Change path of discovery server's SecurityConfig

--- discovery-server/src/main/java/com/ecommerce/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/ecommerce/discoveryserver/config/SecurityConfig.java
@@ -1,11 +1,10 @@
-package config;
+package com.ecommerce.discoveryserver.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
@@ -21,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
         authenticationManagerBuilder.inMemoryAuthentication()
-//                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
                 .withUser("eureka").password("password")
                 .authorities("USER");
     }
