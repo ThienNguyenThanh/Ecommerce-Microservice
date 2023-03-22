fa4d3173333f14da1cca9605139e9a80dca5a6d3
Thien Thanh <nguyenthanhthien0611@gmail.com>
2023-03-22 14:16:53 +0700

    Implement product handler

--- src/frontend/handler.go
+++ src/frontend/handler.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+
+	pb "microservices/frontend/genproto"
 )
 
 var tpl = template.Must(template.ParseGlob("templates/*.html"))
@@ -13,15 +15,49 @@ func (fe *frontendServer) headerHandler(w http.ResponseWriter, r *http.Request) 
 	tpl.ExecuteTemplate(w, "header", nil)
 }
 
+func (fe *frontendServer) productHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		panic(fmt.Sprintf("Can not load product %v", id))
+	}
+
+	productRetrieved, err := fe.getProduct(r.Context(), id)
+	if err != nil {
+		panic(fmt.Sprintf("%v: Can not retrieve product", err))
+	}
+
+	product := struct {
+		Item  *pb.Product
+		Price *pb.Money
+	}{productRetrieved, productRetrieved.GetPriceUsd()}
+	tpl.ExecuteTemplate(w, "product", map[string]interface{}{
+		"product": product,
+	})
+}
+
 func (fe *frontendServer) homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Context())
+
+	products, err := fe.getProducts(r.Context())
+	if err != nil {
+		panic(fmt.Sprintf("%v: Can not load product", err))
+	}
+
+	type productView struct {
+		Item  *pb.Product
+		Price *pb.Money
+	}
+	productList := make([]productView, len(products))
+	for idx, p := range products {
+		productList[idx] = productView{p, p.GetPriceUsd()}
+	}
 	tpl.ExecuteTemplate(w, "home", map[string]interface{}{
 		// "session_id":        sessionID(r),
 		// "request_id":        r.Context().Value(ctxKeyRequestID{}),
 		// "user_currency":     currentCurrency(r),
 		"show_currency": true,
 		"currencies":    "VND",
-		// "products":          ps,
+		"products":      productList,
 		// "cart_size":         cartSize(cart),
 		// "banner_color":      os.Getenv("BANNER_COLOR"), // illustrates canary deployments
 		// "ad":                fe.chooseAd(r.Context(), []string{}, log),
@@ -31,3 +67,25 @@ func (fe *frontendServer) homeHandler(w http.ResponseWriter, r *http.Request) {
 		// "deploymentDetails": deploymentDetailsMap,
 	})
 }
+
+// func renderMoney(money *pb.Money) string {
+// 	currencyLogo := renderCurrencyLogo(money.GetCurrencyCode())
+// 	return fmt.Sprintf("%s%d.%02d", currencyLogo, money.GetUnits(), money.GetNanos()/10000000)
+// }
+
+// func renderCurrencyLogo(currencyCode string) string {
+// 	logos := map[string]string{
+// 		"USD": "$",
+// 		"CAD": "$",
+// 		"JPY": "¥",
+// 		"EUR": "€",
+// 		"TRY": "₺",
+// 		"GBP": "£",
+// 	}
+
+// 	logo := "$" //default
+// 	if val, ok := logos[currencyCode]; ok {
+// 		logo = val
+// 	}
+// 	return logo
+// }
--- src/frontend/main.go
+++ src/frontend/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
 
 type Header struct {
@@ -11,16 +16,75 @@ type Header struct {
 	User  string
 }
 
+var (
+	whitelistedCurrencies = map[string]bool{
+		"USD": true,
+		"EUR": true,
+		"CAD": true,
+		"JPY": true,
+		"GBP": true,
+		"TRY": true}
+)
+
 type frontendServer struct {
+	productCatalogServiceAddress string
+	productCatalogServiceConn    *grpc.ClientConn
 }
 
 func main() {
 
+	ctx := context.Background()
+	srvPort := "3080"
 	svc := new(frontendServer)
 
+	mustMapEnv(&svc.productCatalogServiceAddress, "PRODUCT_CATALOG_SERVICE_ADDR")
+	mustConnGRPC(ctx, &svc.productCatalogServiceConn, svc.productCatalogServiceAddress)
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", svc.homeHandler)
 	http.HandleFunc("/header", svc.headerHandler)
+	http.HandleFunc("/product", svc.productHandler)
 
-	fmt.Println("Start server at port 3000")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	fmt.Println("Start server at port " + srvPort)
+	log.Fatal(http.ListenAndServe("localhost:"+srvPort, nil))
+
+	// viperenv := mustMapEnv("STRONGEST_AVENGER")
+
+	// fmt.Printf("viper : %s = %s \n", "STRONGEST_AVENGER", viperenv)
+}
+
+func mustMapEnv(target *string, envKey string) {
+	viper.SetConfigFile(".env")
+
+	// Find and read the config file
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		log.Fatalf("Error while reading config file %s", err)
+	}
+
+	value, ok := viper.Get(envKey).(string)
+
+	if !ok {
+		log.Fatalf("Invalid type assertion")
+	}
+	// return value
+	*target = value
+
+	//   v := os.Getenv(envKey)
+	// 	if v == "" {
+	// 		panic(fmt.Sprintf("environment variable %q not set", envKey))
+	// 	}
+}
+
+func mustConnGRPC(ctx context.Context, conn **grpc.ClientConn, addr string) {
+	var err error
+	ctx, cancel := context.WithTimeout(ctx, time.Second*3)
+	defer cancel()
+	*conn, err = grpc.DialContext(ctx, addr,
+		grpc.WithInsecure())
+	if err != nil {
+		panic(fmt.Sprintf("%v grpc: failed to connect %s", err, addr))
+	}
 }
